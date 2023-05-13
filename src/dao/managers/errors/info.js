ff9e1a592b45f811fad252869034f7a21d75d398
LeanApa <lea.apagro@gmail.com>
2023-05-13 00:38:34 -0300

    Aniado error info de products

--- src/dao/managers/errors/info.js
+++ src/dao/managers/errors/info.js
@@ -6,3 +6,16 @@ export const generateUserErrorInfo = (user) =>{
     * email: need to be a string, received ${user.email}`
 
 }
+
+export const generateProductErrorInfo = (product) =>{
+    return `One or more properties were incomplete or not valid.
+    List of required properties:
+    * title: need to be a string, received ${product.title}
+    * description: need to be a string, received ${product.description}
+    * code: need to be a string, received ${product.code}
+    * price: need to be a number, received ${product.price}
+    * stock: need to be a number, received ${product.stock}
+    * category: need to be a string, received ${product.category}
+    * thumbnails: need to be a string, received ${product.thumbnails}`
+
+}
