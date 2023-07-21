a9b3070ad667a079c50535ffc10984a25c51e524
Leandro Apablaza <lean@MacBook-Air-de-Leandro.local>
2023-07-21 19:57:09 -0300

    aniado envio de mail por producto eliminado de usuario premium

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,5 +1,7 @@
 import e from "express";
 import { productService, socketServer } from "../app.js";
+import { userModel } from "../dao/models/users.model.js";
+import { transporter } from "../utils.js";
 
 export const getProducts = async (req, res) => {
   try {
@@ -115,15 +117,32 @@ export const deleteProduct = async (req, res) => {
     const {user} = req.user;
     let respuesta = "";
     const producto = await productService.getProductById(pid);
+    const isPremium = await userModel.findOne({email: producto.owner});
     if(user.rol !== 'premium'){
       respuesta = await productService.deleteProduct(pid);
     }else if(producto.owner === user.email){
-      await productService.deleteProduct(pid); 
+      await productService.deleteProduct(pid);
       respuesta = "Producto eliminado";
       res.send({message: respuesta});
     }else{
       respuesta = "Este producto no te pertenece";
       res.send({message: respuesta});
+    }
+    console.log("El isPremium es: ", isPremium);
+    if(isPremium.rol === 'premium'){
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+      await transporter.sendMail({
+        from:'TiendaRopa <lea.apagro@gmail.com>',
+        to: isPremium.email,
+        subject:'Producto eliminado',
+        html:`
+        <div>
+            <h1>Producto eliminado</h1>
+            <p>Su producto ${pid} ha sido eliminado por ${user.email}</p>
+        </div>`,
+        attachments:[]
+      }); 
+      process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "1";
     }
     socketServer.emit("deleteProduct", () => {
       req.logger.info(respuesta);
