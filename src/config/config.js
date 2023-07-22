a2f35b37e910200d2b43fc7e71b85678d3f88ce8
Leandro Apablaza <lean@MacBook-Air-de-Leandro.local>
2023-07-21 21:53:34 -0300

    Cambios menores

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  port: process.env.PORT,
+  port: process.env.PORT || 8080,
   mongoUrl: process.env.MONGODB_URL,
   adminName: process.env.ADMIN_NAME,
   adminPassword: process.env.ADMIN_PASSWORD
